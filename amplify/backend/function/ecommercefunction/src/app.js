28e5013300c727a6623b1cc18e9c1f651235b09f
Pob <590650@gmail.com>
2020-09-03 01:50:43 +0700

    WIP in lambda function

--- amplify/backend/function/ecommercefunction/src/app.js
+++ amplify/backend/function/ecommercefunction/src/app.js
@@ -6,7 +6,6 @@ or in the "license" file accompanying this file. This file is distributed on an 
 See the License for the specific language governing permissions and limitations under the License.
 */
 
-
 /* Amplify Params - DO NOT EDIT
 	AUTH_AMPLIFYECOMMERCE3B5D64E7_USERPOOLID
 	STORAGE_PRODUCTTABLE_ARN
@@ -16,6 +15,47 @@ Amplify Params - DO NOT EDIT */
 var express = require('express')
 var bodyParser = require('body-parser')
 var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const AWS = require('aws-sdk')
+const { v4: uuid } = require('uuid')
+
+const cognito = new AWS.CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' })
+const userpoolId = process.env.AUTH_AMPLIFYECOMMERCE3B5D64E7_USERPOOLID
+
+const region = process.env.REGION
+const ddb_table_name = process.env.STORAGE_PRODUCTTABLE_NAME
+const docClient = new AWS.DynamoDB.DocumentClient({ region })
+
+async function getGroupsForUser(event) {
+  // API gateway event
+  let userSub = event.requestContext.identity.cognitoAuthenticationProvider.split(
+    ':CognitoSignIn:'
+  )[1]
+  let userParams = {
+    UserPoolId: userpoolId,
+    Filter: `sub = "${userSub}"`,
+  }
+  let userData = await cognito.listUsers(userParams).promise()
+  const user = userData.Users[0]
+  const groupParams = {
+    UserPoolId: userpoolId,
+    Username: user.Username,
+  }
+  const groupData = await cognito.adminListGroupsForUser(groupParams).promise()
+  return groupData
+}
+
+async function canPerformAction(event, group) {
+  if (!event.requestContext.identity.cognitoAuthenticationProvider) {
+    throw new Error()
+  }
+  const groupData = await getGroupsForUser(event)
+  const groupsForUser = groupData.Groups.map((group) => group.GroupName)
+  if (groupsForUser.includes(group)) {
+    return
+  } else {
+    throw new Error('user not in group, cannot perform action..')
+  }
+}
 
 // declare a new express app
 var app = express()
@@ -23,72 +63,71 @@ app.use(bodyParser.json())
 app.use(awsServerlessExpressMiddleware.eventContext())
 
 // Enable CORS for all methods
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
-});
-
+})
 
 /**********************
  * Example get method *
  **********************/
 
-app.get('/products', function(req, res) {
+app.get('/products', function (req, res) {
   // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
-});
+  res.json({ success: 'get call succeed!', url: req.url })
+})
 
-app.get('/products/*', function(req, res) {
+app.get('/products/*', function (req, res) {
   // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
-});
+  res.json({ success: 'get call succeed!', url: req.url })
+})
 
 /****************************
-* Example post method *
-****************************/
+ * Example post method *
+ ****************************/
 
-app.post('/products', function(req, res) {
+app.post('/products', function (req, res) {
   // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
-});
+  res.json({ success: 'post call succeed!', url: req.url, body: req.body })
+})
 
-app.post('/products/*', function(req, res) {
+app.post('/products/*', function (req, res) {
   // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
-});
+  res.json({ success: 'post call succeed!', url: req.url, body: req.body })
+})
 
 /****************************
-* Example put method *
-****************************/
+ * Example put method *
+ ****************************/
 
-app.put('/products', function(req, res) {
+app.put('/products', function (req, res) {
   // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
-});
+  res.json({ success: 'put call succeed!', url: req.url, body: req.body })
+})
 
-app.put('/products/*', function(req, res) {
+app.put('/products/*', function (req, res) {
   // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
-});
+  res.json({ success: 'put call succeed!', url: req.url, body: req.body })
+})
 
 /****************************
-* Example delete method *
-****************************/
+ * Example delete method *
+ ****************************/
 
-app.delete('/products', function(req, res) {
+app.delete('/products', function (req, res) {
   // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
-});
+  res.json({ success: 'delete call succeed!', url: req.url })
+})
 
-app.delete('/products/*', function(req, res) {
+app.delete('/products/*', function (req, res) {
   // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
-});
+  res.json({ success: 'delete call succeed!', url: req.url })
+})
 
-app.listen(3000, function() {
-    console.log("App started")
-});
+app.listen(3000, function () {
+  console.log('App started')
+})
 
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
